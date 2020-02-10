2aa25ca808c6648c62684ff55619ea76c5a22705
prakash789 <prakshchandra77@gmail.com>
2020-02-10 13:35:13 +0530

    added hello viewset

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -3,3 +3,4 @@ from rest_framework import serializers
 class HelloSerializers(serializers.Serializer):
     """serializers for test api view"""
     name=serializers.CharField(max_length=10)
+    age=serializers.IntegerField(max_value=None, min_value=None)
